a866e9b2fb232453eff0a714709fa6fdf02b1bf7
Praneeta Verma <praneetav06@gmail.com>
2025-04-14 19:33:20 +0530

    Created routes for upload controller

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express"; // change type to module in package.json for import or require it
 import Connection from "./database/db.js";
 import dotenv from "dotenv";
+import router from "./routes/api.js";
 dotenv.config();
 
 const app = express();
 const PORT = 9000;
+
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log("server is running on port ", PORT);
