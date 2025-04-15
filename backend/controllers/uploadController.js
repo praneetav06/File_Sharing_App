13b78f57bab1bbdf9dd3bec93043f4cb4b8dc8c1
Praneeta Verma <praneetav06@gmail.com>
2025-04-15 17:49:43 +0530

    upload controller functionality and DB Model setup

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,6 +1,16 @@
-const uploadController = (req, res) => {
+const uploadController = async (req, res) => {
   try {
     // functionality to upload file
+    console.log(req.file);
+    const fileObject = {
+      path: req.file.path,
+      name: req.file.originalname,
+    };
+    const file = await fileModel.create(fileObject);
+    console.log(file);
+    return res
+      .status(200)
+      .json({ path: `http://localhost:9000/files/${file._id}` });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
---
+++ backend/models/fileModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+  path: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+const fileModel = mongoose.model("files", fileSchema);
+export default fileSchema;
