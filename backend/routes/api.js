f697c527024e6a9ed1ebf8a52c4f21d515e1a76e
Praneeta Verma <praneetav06@gmail.com>
2025-04-15 15:02:04 +0530

    created middleware

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,9 @@
 import express from "express";
 import uploadController from "../controllers/uploadController";
+import storage from "../middlewares/upload";
 
 const router = express.Router();
 
-router.post("/upload", uploadController);
+router.post("/upload", storage.single("file"), uploadController);
 
 export default router;
