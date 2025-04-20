8a589e00ea1ab177076666b2241fcdc2f7a13cf3
Praneeta Verma <praneetav06@gmail.com>
2025-04-20 16:58:47 +0530

    fixing render error

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 //to serve static files like html, css and js
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
