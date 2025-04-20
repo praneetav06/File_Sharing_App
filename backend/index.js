b29319b90442cf9c8d562ea39a70618bc9a7fc76
Praneeta Verma <praneetav06@gmail.com>
2025-04-20 15:28:28 +0530

    changed routes for serving static files

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import Connection from "./database/db.js";
 import dotenv from "dotenv";
 import router from "./routes/api.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,14 @@ const PORT = 9000;
 app.use(cors());
 
 app.use("/", router);
+
+const __dirname = path.resolve();
+//to serve static files like html, css and js
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("server is running on port ", PORT);
